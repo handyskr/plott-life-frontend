6bebfc6964101c0366647edd0e2465d7e6b71c43
Nine <nine.park@handys.co.kr>
2025-09-09 17:31:11 +0900

    [TF-119] sitemap.xml & robots.txt 설정

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -5,11 +5,19 @@ import preact from "@astrojs/preact";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
+const liveURL = "https://life.plott.co.kr";
 const { SITE } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 export default defineConfig({
   site: SITE,
-  integrations: [preact(), sitemap()],
+  integrations: [
+    preact(),
+    sitemap({
+      filter: (page) =>
+        page.startsWith(liveURL) &&
+        !page.startsWith(`${liveURL}/theme`),
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
