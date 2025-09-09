ce833c39ea62bd647b2fe1231f709636a969ec9e
Nine <nine.park@handys.co.kr>
2025-09-09 17:16:58 +0900

    [TF-119] eslint 설정

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -2,13 +2,14 @@ import { loadEnv } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import node from "@astrojs/node";
 import preact from "@astrojs/preact";
+import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
 const { SITE } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 export default defineConfig({
   site: SITE,
-  integrations: [preact()],
+  integrations: [preact(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
