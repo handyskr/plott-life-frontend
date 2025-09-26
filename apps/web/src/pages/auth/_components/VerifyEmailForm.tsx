18548ebee8f5754c794c8a6481493d328b690b89
Nine <nine.park@handys.co.kr>
2025-09-26 09:31:42 +0900

    [TF-156] 이메일 인증 코드 에러 메시지 수정

--- apps/web/src/pages/auth/_components/VerifyEmailForm.tsx
+++ apps/web/src/pages/auth/_components/VerifyEmailForm.tsx
@@ -42,7 +42,7 @@ export const VerifyEmailForm = (props: Props) => {
 
       switch ((error as ActionError)?.code) {
         default:
-          toast.show({ type: 'default', message: error?.message || '알 수 없는 에러가 발생했습니다.' });
+          toast.show({ type: 'default', message: error?.message || '잘못된 코드를 입력하셨습니다.' });
           console.error(error);
           break;
       }
